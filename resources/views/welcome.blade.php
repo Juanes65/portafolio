8631df071c444d9d390792f3cd17014310d16482
Juan Esteban <desarrollo2@historyclinical.com>
2024-08-29 09:16:09 -0500

    broma

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    diego estuvo aqui 
                 </div>
 
                 <div class="links">
